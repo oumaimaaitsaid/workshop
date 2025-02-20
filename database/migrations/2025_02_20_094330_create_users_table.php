dac73422a859d67447ac81da0c41e3b97eebc66c
amdymstfa <amdymoustapha011@gmail.com>
2025-02-20 11:23:25 +0100

    done : some changes

--- database/migrations/2025_02_20_094330_create_users_table.php
+++ database/migrations/2025_02_20_094330_create_users_table.php
@@ -9,18 +9,19 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up()
+{
+    if (!Schema::hasTable('users')) {
         Schema::create('users', function (Blueprint $table) {
-            Schema::create('users', function (Blueprint $table) {
-                $table->id();
-                $table->string('name');
-                $table->string('email')->unique();
-                $table->string('password');
-                $table->timestamps();
-            });
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
         });
     }
+}
+
 
     /**
      * Reverse the migrations.
